c619cf2bc42e86fd9c05d1d82249af73f8415228
Shamn001 <shamintv91@gmail.com>
2025-06-15 20:00:48 +0530

    initial commit

---
+++ test1.dart
@@ -0,0 +1,38 @@
+          //2
+//1
+void main(List<String> args) {
+  List<int> numbers = [10, 20, 30, 40];
+  numbers.add(50);
+  print("1. $numbers");
+  numbers.removeAt(1);
+  print("2. $numbers");
+  //4
+  if(numbers.contains(30)){
+    print("3. 30 is in the list");
+  }
+  else{
+    print("3. 30 is not in the list");
+  }
+  //5
+  int l=numbers.length;
+  print("4. $l");
+  int fst=numbers.first;
+  int lst=numbers.last;
+  print("5. First = $fst ,Last = $lst");
+  //6
+  String st=numbers.toString();
+  print("6. $st");
+  //7
+  if(numbers.isEmpty){
+    print("7. The List is Empty");
+  }
+  else{
+    print("7. Listn is not empty");
+  }
+  //8
+  numbers.sort();
+  print("8. $numbers");
+  //9
+  var rev=numbers.reversed;
+  print("9. $rev");
+} 

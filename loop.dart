6a1efcffc2a989ced6c6fd178d6b01f7bf68c270
Shamn001 <shamintv91@gmail.com>
2025-06-19 10:22:33 +0530

    first commit

---
+++ loop.dart
@@ -0,0 +1,17 @@
+import "dart:io";
+void main(List<String> args) {
+  List<int> a=[1, 2, 3, 4, 5];
+  for(int i=0;i<a.length;i++){
+    if(a[i]%2==1){
+      print(a[i]);
+    }
+  }
+  print("\nQn.2\n");
+  //2
+  int sum=0;
+  List<int> nums = [5,10,15];
+  for(int i=0;i<nums.length;i++){
+    sum=sum+nums[i];
+  }
+  print(sum);
+}
--- test1.dart
+++ test1.dart
@@ -4,18 +4,20 @@ void main(List<String> args) {
   List<int> numbers = [10, 20, 30, 40];
   numbers.add(50);
   print("1. $numbers");
+  //2
   numbers.removeAt(1);
   print("2. $numbers");
-  //4
+  //3
   if(numbers.contains(30)){
     print("3. 30 is in the list");
   }
   else{
     print("3. 30 is not in the list");
   }
-  //5
+  //4
   int l=numbers.length;
   print("4. $l");
+  //5
   int fst=numbers.first;
   int lst=numbers.last;
   print("5. First = $fst ,Last = $lst");
